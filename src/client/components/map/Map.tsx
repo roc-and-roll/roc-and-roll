53b165c686b903fa7ad732d052eb798e7606f60a
Christian Flach <cmfcmf.flach@gmail.com>
2021-05-09 18:16:28 +0200

    Snap map objects to grid

--- src/client/components/map/Map.tsx
+++ src/client/components/map/Map.tsx
@@ -135,6 +135,7 @@ export const RRMapView = React.memo<{
   backgroundColor: RRColor;
   transformRef: React.MutableRefObject<Matrix>;
   onMoveMapObjects: (d: RRPoint) => void;
+  onStopMoveMapObjects: () => void;
   onSetHP: (tokenId: RRCharacterID, hp: number) => void;
   handleKeyDown: (event: KeyboardEvent) => void;
   players: EntityCollection<RRPlayer>;
@@ -151,6 +152,7 @@ export const RRMapView = React.memo<{
   onSetHP,
   handleKeyDown,
   onMoveMapObjects,
+  onStopMoveMapObjects,
   transformRef,
   players,
   measurePathDebounce,
@@ -488,6 +490,7 @@ export const RRMapView = React.memo<{
   const handleMouseUp = useRecoilCallback(
     ({ snapshot }) => (e: MouseEvent) => {
       if (mouseActionRef.current === MouseAction.MOVE_MAP_OBJECT) {
+        onStopMoveMapObjects();
         setMeasurePath([]);
         dragStartIdRef.current = null;
       }
@@ -521,6 +524,7 @@ export const RRMapView = React.memo<{
       setSelectedMapObjectIds,
       toolHandler,
       transformRef,
+      onStopMoveMapObjects,
     ]
   );
 
