c3945e36d4779610d6c1963fb7716af0f797fdbd
Wanda Baltzer <wanda.baltzer@student.hpi.de>
2021-05-09 15:50:08 +0200

    styling

--- src/client/components/DiceInterface.tsx
+++ src/client/components/DiceInterface.tsx
@@ -1,9 +1,64 @@
 import React, { useState } from "react";
 import { Button } from "./ui/Button";
+import { logEntryDiceRollAdd } from "../../shared/actions";
+import { RRDice, RRModifier } from "../../shared/state";
+import { useMyself } from "../myself";
+import { useServerDispatch } from "../state";
+import { roll } from "../roll";
 
 export function DiceInterface() {
   const [diceTypes, setDiceTypes] = useState<string[]>([]);
   const [boni, setBoni] = useState<number>(0);
+  const myself = useMyself();
+  const dispatch = useServerDispatch();
+
+  const doRoll = () => {
+    const boniString = boni >= 0 ? "+" + boni.toString() : boni.toString();
+    const rollString = diceTypes.join("+") + boniString;
+
+    const regex = /(^| *[+-] *)(?:(\d*)(d|a|i)(\d+)|(\d+))/g;
+    const dice = [...rollString.matchAll(regex)].map(
+      ([_, sign, diceCount, die, dieFaces, mod]): RRDice | RRModifier => {
+        const negated = sign?.trim() === "-";
+        if (diceCount !== undefined && dieFaces !== undefined) {
+          // die
+          const faces = parseInt(dieFaces);
+          const count =
+            diceCount === "" ? (die === "d" ? 1 : 2) : parseInt(diceCount);
+          return roll({
+            count,
+            faces,
+            modified:
+              die === "a" ? "advantage" : die === "i" ? "disadvantage" : "none",
+            negated,
+          });
+        } else if (mod) {
+          // mod
+          const modifier = parseInt(mod) * (negated ? -1 : 1);
+          return {
+            type: "modifier",
+            damageType: null,
+            modifier,
+          };
+        }
+        throw new Error();
+      }
+    );
+
+    if (dice.length) {
+      dispatch(
+        logEntryDiceRollAdd({
+          silent: false,
+          playerId: myself.id,
+          payload: { dice, rollType: null },
+        })
+      );
+      setDiceTypes([]);
+      setBoni(0);
+    } else {
+      alert("Please follow the regex: " + regex.toString());
+    }
+  };
 
   function addDiceType(diceType: string) {
     const index = diceTypes.findIndex((element) => element.includes(diceType));
@@ -35,7 +90,7 @@ export function DiceInterface() {
   return (
     <>
       <div id="pane">
-        <div style={{ width: "66%" }}>
+        <div>
           <table
             style={{
               width: "100%",
@@ -48,35 +103,35 @@ export function DiceInterface() {
               <th style={{ width: "33%" }}>Bonus</th>
             </tr>
             <tr>
-              <td>
-                <Button onClick={() => addDiceType("d4")}>d4</Button> <br />
-                <Button onClick={() => addDiceType("d6")}>d6</Button> <br />
-                <Button onClick={() => addDiceType("d8")}>d8</Button> <br />
-                <Button onClick={() => addDiceType("d10")}>d10</Button> <br />
-                <Button onClick={() => addDiceType("d12")}>d12</Button> <br />
+              <td className="buttons-full-width">
+                <Button onClick={() => addDiceType("d4")}>d4</Button>
+                <Button onClick={() => addDiceType("d6")}>d6</Button>
+                <Button onClick={() => addDiceType("d8")}>d8</Button>
+                <Button onClick={() => addDiceType("d10")}>d10</Button>
+                <Button onClick={() => addDiceType("d12")}>d12</Button>
                 <Button onClick={() => addDiceType("d20")}>d20</Button>
               </td>
-              <td>
-                <td>
-                  <Button onClick={() => addBonus(-1)}>-1</Button> <br />
-                  <Button onClick={() => addBonus(1)}>+1</Button> <br />
-                  <Button onClick={() => addBonus(2)}>+2</Button> <br />
-                  <Button onClick={() => addBonus(3)}>+3</Button> <br />
-                  <Button onClick={() => addBonus(4)}>+4</Button> <br />
-                </td>
-                <td>
-                  <Button onClick={() => addBonus(5)}>+5</Button> <br />
-                  <Button onClick={() => addBonus(6)}>+6</Button> <br />
-                  <Button onClick={() => addBonus(7)}>+7</Button> <br />
-                  <Button onClick={() => addBonus(8)}>+8</Button> <br />
+              <td className="buttons-half-width">
+                <div>
+                  <Button onClick={() => addBonus(-1)}>-1</Button>
+                  <Button onClick={() => addBonus(1)}>+1</Button>
+                  <Button onClick={() => addBonus(2)}>+2</Button>
+                  <Button onClick={() => addBonus(3)}>+3</Button>
+                  <Button onClick={() => addBonus(4)}>+4</Button>
+                </div>
+                <div>
+                  <Button onClick={() => addBonus(5)}>+5</Button>
+                  <Button onClick={() => addBonus(6)}>+6</Button>
+                  <Button onClick={() => addBonus(7)}>+7</Button>
+                  <Button onClick={() => addBonus(8)}>+8</Button>
                   <Button onClick={() => addBonus(9)}>+9</Button>
-                </td>
+                </div>
               </td>
 
               <td>
-                <Button style={{ width: "100%", height: "150px" }}>
+                <Button className="roll-it-button" onClick={doRoll}>
                   <p>ROLL IT</p>
-                  <p>{diceTypes.join("+")}</p>
+                  <p>{diceTypes.join(" + ")}</p>
                   <div>
                     {boni >= 0 ? "+" + boni.toString() : boni.toString()}
                   </div>
