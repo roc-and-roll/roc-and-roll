1b1225ca81ff5f570399c1eee816cdef773f178e
Christian Flach <cmfcmf.flach@gmail.com>
2021-05-13 13:18:44 +0200

    Allow to move individual people across maps

--- src/client/components/Maps.tsx
+++ src/client/components/Maps.tsx
@@ -1,7 +1,15 @@
+import clsx from "clsx";
 import React from "react";
+import { useDrag, useDrop } from "react-dnd";
 import { mapAdd, playerUpdate } from "../../shared/actions";
 import { randomColor } from "../../shared/colors";
-import { entries } from "../../shared/state";
+import {
+  EntityCollection,
+  entries,
+  RRMap,
+  RRPlayer,
+  RRPlayerID,
+} from "../../shared/state";
 import { useMyself } from "../myself";
 import { useServerDispatch, useServerState } from "../state";
 import { GMArea } from "./GMArea";
@@ -15,43 +23,16 @@ export function Maps() {
 
   return (
     <GMArea>
-      <ul>
+      <ul role="list" className="maps">
         {entries(maps).map((map) => (
-          <li key={map.id}>
-            <strong>{map.name}</strong>
-            <br />
-            {entries(players)
-              .filter((player) => player.currentMap === map.id)
-              .map((player) => player.name)
-              .join(", ")}
-            <div>
-              <Button
-                onClick={() =>
-                  players.ids.map((id) =>
-                    dispatch(
-                      playerUpdate({ id, changes: { currentMap: map.id } })
-                    )
-                  )
-                }
-              >
-                move everyone here
-              </Button>
-              <Button
-                onClick={() =>
-                  dispatch(
-                    playerUpdate({
-                      id: myself.id,
-                      changes: { currentMap: map.id },
-                    })
-                  )
-                }
-              >
-                go here
-              </Button>
-            </div>
-          </li>
+          <MapListEntry
+            key={map.id}
+            map={map}
+            myself={myself}
+            players={players}
+          />
         ))}
-        <li>
+        <li className="maps-create">
           <Button
             onClick={() => {
               const name = prompt("Enter the name of the map")?.trim();
@@ -80,5 +61,119 @@ export function Maps() {
         </li>
       </ul>
     </GMArea>
+  );
+}
+
+function MapListEntry({
+  map,
+  myself,
+  players: allPlayers,
+}: {
+  map: RRMap;
+  myself: RRPlayer;
+  players: EntityCollection<RRPlayer>;
+}) {
+  const dispatch = useServerDispatch();
+  const players = entries(allPlayers).filter(
+    (player) => player.currentMap === map.id
+  );
+  const isMyCurrentMap = myself.currentMap === map.id;
+
+  const [{ canDropAndHovered, canDrop }, dropRef] = useDrop<
+    { id: RRPlayerID },
+    void,
+    { canDropAndHovered: boolean; canDrop: boolean }
+  >(
+    () => ({
+      accept: "maps-player",
+      drop: (item) => {
+        dispatch(
+          playerUpdate({ id: item.id, changes: { currentMap: map.id } })
+        );
+      },
+      collect: (monitor) => ({
+        canDropAndHovered: monitor.isOver() && monitor.canDrop(),
+        canDrop: monitor.canDrop(),
+      }),
+      canDrop: (item) => players.every((player) => player.id !== item.id),
+    }),
+    [dispatch, map.id, players]
+  );
+
+  return (
+    <li>
+      <h3>
+        {map.name}
+        {isMyCurrentMap && (
+          <>
+            {" "}
+            <span className="maps-you-are-here">you are here</span>
+          </>
+        )}
+      </h3>
+      <div
+        className={clsx(
+          "maps-players",
+          canDropAndHovered && "maps-players-hovered",
+          canDrop && "maps-players-can-drop"
+        )}
+        ref={dropRef}
+      >
+        {players.map((player) => (
+          <MapPlayer key={player.id} player={player} />
+        ))}
+        {players.length === 0 && (
+          <div className="maps-no-players">drop players here</div>
+        )}
+      </div>
+      <div>
+        {allPlayers.ids.length !== players.length && (
+          <Button
+            onClick={() =>
+              dispatch(
+                allPlayers.ids.map((id) =>
+                  playerUpdate({ id, changes: { currentMap: map.id } })
+                )
+              )
+            }
+          >
+            move everyone here
+          </Button>
+        )}
+        {!isMyCurrentMap && (
+          <Button
+            onClick={() =>
+              dispatch(
+                playerUpdate({
+                  id: myself.id,
+                  changes: { currentMap: map.id },
+                })
+              )
+            }
+          >
+            go here
+          </Button>
+        )}
+      </div>
+    </li>
+  );
+}
+
+function MapPlayer({ player }: { player: RRPlayer }) {
+  const [, dragRef] = useDrag(
+    () => ({
+      type: "maps-player",
+      item: { id: player.id },
+      options: {
+        dropEffect: "move",
+      },
+    }),
+    [player.id]
+  );
+
+  return (
+    <span key={player.id} className="map-player" ref={dragRef}>
+      {player.name}
+    </span>
   );
 }
