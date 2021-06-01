59ea8ef53ec44c83613f6039e3a0ab1b15c1bc46
Christian Flach <cmfcmf.flach@gmail.com>
2021-06-01 22:55:49 +0200

    Major fixes to sound player
    - sounds are correctly seeked to, even when they are playing in the nth
    loop
    - sounds are not played multiple times in some edge cases

--- src/client/sound.tsx
+++ src/client/sound.tsx
@@ -81,11 +81,16 @@ type SoundState =
 export function useRRComplexSound(
   url: string,
   soundVolume: number,
-  // if true, the sound does not need to be downloaded in its entirety before it
-  // starts playing
-  stream: boolean,
-  loop: boolean = false
-): readonly [(startInSeconds?: number) => void, () => void, SoundState] {
+  {
+    stream: html5,
+    loop,
+  }: {
+    // if true, the sound does not need to be downloaded in its entirety before it
+    // starts playing
+    stream: boolean;
+    loop: boolean;
+  }
+): readonly [(startedAt: number) => void, () => void, () => void, SoundState] {
   const urlRef = useLatest(url);
   const loopRef = useLatest(loop);
 
@@ -111,10 +116,41 @@ export function useRRComplexSound(
   const [state, setState] = useState<SoundState>("stopped");
   const lastUrlRef = useRef("");
 
+  const durationRef = useRef<number | undefined>(undefined);
+  const soundIdRef = useRef<number | undefined>(undefined);
+  const startedAtRef = useRef<number | undefined>(undefined);
+
+  const _play = useCallback(() => {
+    if (startedAtRef.current === undefined) {
+      // When calling play() multiple times, we do not want to queue additional
+      // versions of this sound. Instead, we want to stop playing the
+      // potentially already playing sound before calling play() again.
+      // Therefore we pass the soundId of the previously played sound to
+      // interrupt it and start it again.
+      soundIdRef.current = howlRef.current?.play(soundIdRef.current);
+    } else {
+      if (durationRef.current !== undefined) {
+        soundIdRef.current = howlRef.current?.play(soundIdRef.current);
+        // At least in Chrome, seeking in a looped sound only works when seeking
+        // to an offset <= duration the duration of the song. Therefore, we can
+        // only start playing the song at the correct seek position if we know
+        // the duration of the song.
+        const seekSeconds =
+          ((Date.now() - startedAtRef.current) / 1000) % durationRef.current;
+        howlRef.current?.seek(seekSeconds, soundIdRef.current);
+      }
+    }
+  }, []);
+
   const play = useCallback(
     (startedAt?: number) => {
+      startedAtRef.current = startedAt;
+
+      // Unload the previous sound if the url changes.
       if (lastUrlRef.current !== urlRef.current) {
         howlRef.current?.unload();
+        durationRef.current = undefined;
+        soundIdRef.current = undefined;
         howlRef.current = null;
       }
       lastUrlRef.current = urlRef.current;
@@ -123,36 +159,36 @@ export function useRRComplexSound(
         setState("loading");
         howlRef.current = new Howl({
           src: urlRef.current,
-          html5: stream,
-          // We need to initialize loop, mute, and volume both here and in the
+          html5,
+          autoplay: false,
+          preload: true,
+          // We need to initialize volume, mute, and loop both here and in the
           // effect below, because howlRef.current is null when the effect is
           // executed before calling play for the first time.
-          loop: loopRef.current,
-          mute: globalUserMuteRef.current,
           volume: volumeRef.current,
+          mute: globalUserMuteRef.current,
+          loop: loopRef.current,
           onplay: () => setState("playing"),
           onpause: () => setState("paused"),
           onstop: () => setState("stopped"),
           onend: () => setState("stopped"),
           onloaderror: () => setState("error"),
+          onload: () => {
+            durationRef.current = howlRef.current?.duration();
+            _play();
+          },
           onplayerror: () => {
             // https://github.com/goldfire/howler.js/#mobilechrome-playback
             setState("error-needs-unlock");
-            howlRef.current?.once("unlock", () => {
-              howlRef.current?.play();
-              if (startedAt !== undefined) {
-                howlRef.current?.seek((Date.now() - startedAt) / 1000);
-              }
-            });
+          },
+          onunlock: () => {
+            _play();
           },
         });
       }
-      if (startedAt !== undefined) {
-        howlRef.current.seek((Date.now() - startedAt) / 1000);
-      }
-      howlRef.current.play();
+      _play();
     },
-    [globalUserMuteRef, volumeRef, loopRef, stream, urlRef]
+    [globalUserMuteRef, volumeRef, loopRef, html5, urlRef, _play]
   );
 
   useEffect(() => {
@@ -165,6 +201,10 @@ export function useRRComplexSound(
     howlRef.current?.pause();
   }, []);
 
+  const stop = useCallback(() => {
+    howlRef.current?.stop();
+  }, []);
+
   const setLockedSounds = useSetRecoilState(lockedSoundsAtom);
   const id = useMemo(() => nanoid(), []);
   useEffect(() => {
@@ -172,7 +212,7 @@ export function useRRComplexSound(
     return () => setLockedSounds(updateLockedSounds(id, false));
   }, [state, setLockedSounds, id]);
 
-  return [play, pause, state] as const;
+  return [play, pause, stop, state] as const;
 }
 
 export function SongPlayer() {
@@ -195,12 +235,10 @@ export function SongPlayer() {
 }
 
 export function ActiveSongPlayer({ song }: { song: RRActiveSong }) {
-  const [play] = useRRComplexSound(
-    assetUrl(song.song),
-    song.volume,
-    true,
-    true
-  );
+  const [play] = useRRComplexSound(assetUrl(song.song), song.volume, {
+    stream: true,
+    loop: true,
+  });
 
   useEffect(() => {
     play(song.startedAt);
