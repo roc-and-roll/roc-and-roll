290af60ad4dad41c82801034e3635fb0fbb3a2c6
Corinna Jaschek <corinna.jaschek@gmail.com>
2021-12-08 22:02:35 +0100

    fix typo in state as well

---
+++ src/server/migrations/24_fix_typo.ts
@@ -0,0 +1,37 @@
+import { AbstractMigration } from "../migrations";
+
+export default class extends AbstractMigration {
+  version = 24;
+
+  migrate = (state: any) => {
+    Object.values(
+      state.characters.entities as Record<
+        string,
+        { auras: { visibileWhen?: any; visibleWhen?: any }[] }
+      >
+    ).forEach((character) => {
+      character.auras.forEach((aura) => {
+        if (aura.visibileWhen) {
+          aura.visibleWhen = aura.visibileWhen;
+          delete aura.visibileWhen;
+        }
+      });
+    });
+
+    Object.values(
+      state.characterTemplates.entities as Record<
+        string,
+        { auras: { visibileWhen?: any; visibleWhen?: any }[] }
+      >
+    ).forEach((character) => {
+      character.auras.forEach((aura) => {
+        if (aura.visibileWhen) {
+          aura.visibleWhen = aura.visibileWhen;
+          delete aura.visibileWhen;
+        }
+      });
+    });
+
+    return state;
+  };
+}
