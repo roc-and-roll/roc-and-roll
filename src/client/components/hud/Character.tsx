4e95ef4b0e373249d345da37221d0872881b9d74
Corinna Jaschek <corinna.jaschek@gmail.com>
2022-02-12 12:05:33 +0100

    make map edit buttons usable with combat cards, #166

--- src/client/components/hud/Character.tsx
+++ src/client/components/hud/Character.tsx
@@ -91,7 +91,7 @@ export function CharacterHUD() {
             <CombatCardHUD />
           </div>
         )}
-        <div className="flex flex-col items-center pointer-events-auto min-h-min">
+        <div className="flex flex-col items-center pointer-events-none min-h-min">
           <CurrentCharacter character={character} />
           {character && <AC character={character} />}
           {character && <SpellSave character={character} />}
@@ -307,7 +307,7 @@ function HeroPoint() {
 
   return (
     <div
-      className="border-solid rounded-md w-10 h-10 flex justify-center items-center border-white opacity-80 m-2 cursor-pointer"
+      className="border-solid rounded-md w-10 h-10 flex justify-center items-center border-white opacity-80 m-2 cursor-pointer pointer-events-auto"
       style={{
         borderWidth: "5px",
         boxShadow: myself.hasHeroPoint ? "0 0 5px 1px #fff" : "",
@@ -341,7 +341,7 @@ function HeroPoint() {
 
 function AC({ character }: { character: RRCharacterProps }) {
   return (
-    <div className="relative">
+    <div className="relative pointer-events-auto">
       <FontAwesomeIcon
         icon={faShieldAlt}
         fixedWidth
@@ -356,7 +356,7 @@ function AC({ character }: { character: RRCharacterProps }) {
 
 function SpellSave({ character }: { character: RRCharacterProps }) {
   return (
-    <div className="relative">
+    <div className="relative pointer-events-auto">
       <FontAwesomeIcon
         icon={faMagic}
         fixedWidth
@@ -377,7 +377,7 @@ function CurrentCharacter({
   const [settingsOpen, setSettingsOpen] = useState(false);
   const myself = useMyProps("color");
   return (
-    <div>
+    <div className="pointer-events-auto">
       {settingsOpen && (
         <SettingsDialog onClose={() => setSettingsOpen(false)} />
       )}
