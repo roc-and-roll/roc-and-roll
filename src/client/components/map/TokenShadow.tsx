a5e2476d34c7b1b482f19f16f19350b494bcc8f8
Tom Beckmann <tomjonabc@gmail.com>
2022-02-27 13:34:57 +0100

    animate token shadows

--- src/client/components/map/TokenShadow.tsx
+++ src/client/components/map/TokenShadow.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import * as PIXI from "pixi.js";
 import haloImage from "./halo.png";
-import { Sprite } from "react-pixi-fiber";
+import { Container, Sprite } from "react-pixi-fiber";
 
 export function TokenShadow({
   color,
@@ -13,14 +13,38 @@ export function TokenShadow({
   size: number;
 }) {
   const totalSize = size * 2;
+  const rafId = useRef<number | null>();
+  const spriteRef = useRef<PIXI.Container>(null);
+
+  const step = useCallback(
+    (now: number) => {
+      if (spriteRef.current) {
+        const scale = Math.abs(Math.sin(now * (1 / pulse) * 0.003) * 0.5) + 0.5;
+        spriteRef.current.scale.x = scale;
+        spriteRef.current.scale.y = scale;
+      }
+      rafId.current = requestAnimationFrame(step);
+    },
+    [pulse]
+  );
+
+  useEffect(() => {
+    rafId.current = requestAnimationFrame(step);
+    return () => {
+      if (rafId.current) cancelAnimationFrame(rafId.current);
+    };
+  }, [step]);
+
   return (
-    <Sprite
-      width={totalSize}
-      height={totalSize}
-      x={-size / 2}
-      y={-size / 2}
-      tint={color}
-      texture={PIXI.Texture.from(haloImage)}
-    />
+    <Container ref={spriteRef} x={size / 2} y={size / 2}>
+      <Sprite
+        x={-size}
+        y={-size}
+        width={totalSize}
+        height={totalSize}
+        tint={color}
+        texture={PIXI.Texture.from(haloImage)}
+      />
+    </Container>
   );
 }
