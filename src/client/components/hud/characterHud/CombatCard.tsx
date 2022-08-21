bd50263779a3e16e47ef58566138594714ce05fa
Corinna Jaschek <corinna.jaschek@gmail.com>
2022-08-21 17:05:25 +0200

    remove console logs

--- src/client/components/hud/characterHud/CombatCard.tsx
+++ src/client/components/hud/characterHud/CombatCard.tsx
@@ -52,9 +52,7 @@ export function CombatCardHUD() {
   };
 
   const { setOpen, setSearchString } = useContext(QuickReferenceContext);
-  console.log(monster);
   const lairActions = useLairActions(monster);
-  console.log(lairActions);
 
   if (!monster) return null;
   return (
